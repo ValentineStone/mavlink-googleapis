f5e44ecd8ce117ddfb020253eb3a224c5013e96c
Leonid Romanovsky <sirleonidromanovsky@gmail.com>
2021-03-26 09:22:38 +0300

    reverted to mavlink2((((

--- reorder.js
+++ reorder.js
@@ -4,6 +4,27 @@ const pongTimeout = +process.env.DEVICE_SERVICE_PONG_TIMEOUT
 const spawn = () => {
   const mav2 = new MAVLink20Processor()
 
+  let pongOnTimeout = false
+  const pong = (send) => {
+    if (pongOnTimeout) return
+    if (pongTimeout) {
+      setTimeout(() => pongOnTimeout = false, pongTimeout)
+      pongOnTimeout = true
+    }
+    console.log('pong')
+    send(
+      Uint8Array.from(
+        mav2.send(
+          new mavlink20.messages.command_long(
+            mavlinkId, 1, 0,
+            mav2.MAV_CMD_REQUEST_MESSAGE,
+            mav2.MAVLINK_MSG_ID_PROTOCOL_VERSION
+          )
+        )
+      )
+    )
+  }
+
   const send = (buff, sendBack) => {
     const messages = []
     for (const message of mav2.parseBuffer(buff)) {
@@ -16,29 +37,8 @@ const spawn = () => {
   }
 
   const recv = (buff, callback) => callback(buff)
-  const reset = () => {}
+  const reset = () => { }
   return { send, recv, reset }
-}
-
-let pongOnTimeout = false
-const pong = send => {
-  if (pongOnTimeout) return
-  if (pongTimeout) {
-    setTimeout(() => pongOnTimeout = false, pongTimeout)
-    pongOnTimeout = true
-  }
-  console.log('pong')
-  send(
-    Uint8Array.from(
-      mav2.send(
-        new mavlink20.messages.command_long(
-          mavlinkId, 1, 0,
-          mav2.MAV_CMD_REQUEST_MESSAGE,
-          mav2.MAVLINK_MSG_ID_PROTOCOL_VERSION
-        )
-      )
-    )
-  )
 }
 
 module.exports = spawn
